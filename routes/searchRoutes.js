6c10f8e03cc7896ea215cdc09d13ba9d413fb426
PauRaset <94690926+PauRaset@users.noreply.github.com>
2025-05-09 14:02:53 +0200

    Update searchRoutes.js

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/User"); // Asegúrate de importar tu modelo de usuario
+const User = require("../models/User"); 
 
-// Ruta para buscar usuarios y clubes
 router.get("/api/search", async (req, res) => {
     try {
         const { query } = req.query;
@@ -12,10 +11,9 @@ router.get("/api/search", async (req, res) => {
                 .json({ error: "Se requiere un término de búsqueda" });
         }
 
-        // Busca en la base de datos usuarios cuyo nombre de usuario coincida parcialmente
         const users = await User.find({
             username: { $regex: query, $options: "i" },
-        }).select("username profilePicture role"); // Selecciona solo los campos necesarios
+        }).select("username profilePicture role");
 
         res.json(users);
     } catch (error) {
