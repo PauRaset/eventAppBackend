2427de2137cfb4a230cf2b13ed04798393f33c05
PauRaset <94690926+PauRaset@users.noreply.github.com>
2025-05-09 13:58:55 +0200

    Update Event.js

--- models/Event.js
+++ models/Event.js
@@ -8,10 +8,10 @@ const eventSchema = new mongoose.Schema({
   street: String,
   postalCode: String,
   image: String,
-  categories: [String], // Ahora admite múltiples categorías
+  categories: [String], 
   age: String,
-  dressCode: String, // Nuevo campo para Dress Code
-  price: String, // Nuevo campo para el precio de la entrada
+  dressCode: String,
+  price: String, 
   attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   createdBy: {
     type: mongoose.Schema.Types.ObjectId,
@@ -21,24 +21,3 @@ const eventSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model("Event", eventSchema);
-
-/*const mongoose = require("mongoose");
-
-const eventSchema = new mongoose.Schema({
-  title: String,
-  description: String,
-  date: Date,
-  city: String,
-  street: String,
-  postalCode: String,
-  image: String,
-  category: String,
-  attendees: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  createdBy: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  }, // Nuevo campo
-});
-
-module.exports = mongoose.model("Event", eventSchema);*/
